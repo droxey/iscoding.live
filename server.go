6812dff18cb049e4dd6ea5adec29c59ec6b730a4
Dani Roxberry <dani@bitoriented.com>
2019-01-07 17:54:44 -0800

    [add] user timezone conversion and print data

--- server.go
+++ server.go
@@ -17,10 +17,11 @@ var json = jsoniter.ConfigCompatibleWithStandardLibrary
 // Coder represents a programmer who may
 // or may not be coding at this very moment.
 type Coder struct {
-	Email           string `json:"email"`
-	Username        string `json:"username"`
-	LatestHeartbeat string `json:"last_heartbeat"`
-	LatestProject   string `json:"last_project"`
+	Email           string    `json:"email"`
+	Username        string    `json:"username"`
+	LatestProject   string    `json:"last_project"`
+	Timezone        string    `json:"timezone"`
+	LatestHeartbeat time.Time `json:"last_heartbeat"`
 }
 
 // Team contains an array of Coder to represent
@@ -29,38 +30,62 @@ type Team struct {
 	Coders []Coder `json:"data"`
 }
 
+// timeIn converts UTC time to local time.
+func timeIn(t time.Time, tzName string) time.Time {
+	loc, err := time.LoadLocation(tzName)
+	if err != nil {
+		panic(err)
+	}
+	return t.In(loc)
+}
+
 func main() {
+	// Initialize godotenv for reading secrets stored in .env files.
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
+	// Grab secrets from .env using godotenv.
 	apiKey := os.Getenv("WAKATIME_API_KEY")
 	teamGUID := os.Getenv("WAKATIME_TEAM_GUID")
+
 	url := "https://wakatime.com/api/v1/users/current/teams/" + teamGUID + "/members?api_key=" + apiKey
 
 	client := http.Client{
+		// If you have a large team in Wakatime, it might take a while to return the data.
+		// Set the timeout higher for these requests.
 		Timeout: time.Second * 60,
 	}
 
+	// Use http.NewRequest when you need to specify attributes
+	// of the request. Example: setting custom headers.
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Set a custom header for User-Agent so the
+	// Wakatime API knows who made the request.
 	req.Header.Set("User-Agent", "iscoding.live")
 
+	// Execute the GET request.
 	res, getErr := client.Do(req)
 	if getErr != nil {
 		log.Fatal(getErr)
 	}
 
+	// Response data is available in response.Body
+	// Read until error or EOF.
 	body, readErr := ioutil.ReadAll(res.Body)
 	if readErr != nil {
 		log.Fatal(readErr)
 	}
 
+	// Create a new Team to store the data we're interested in.
 	team := &Team{}
+
+	// Unmarshal the JSON data to populate the team object.
 	parseErr := json.Unmarshal([]byte(body), &team)
 
 	if parseErr != nil {
@@ -68,6 +93,11 @@ func main() {
 		return
 	}
 
-	fmt.Printf("%v\n", team)
+	// Print the resulting object.
+	// Convert LatestHeartbeat to the current user's timezone.
+	for _, coder := range team.Coders {
+		coder.LatestHeartbeat = timeIn(coder.LatestHeartbeat, coder.Timezone)
+		fmt.Printf("%v: %v \n\n", coder.Email, coder.LatestHeartbeat)
+	}
 }
 
